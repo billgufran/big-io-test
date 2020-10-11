d4bac5ffcc3fce6df6215eecc74b007d626e6f2f
billgufran <billgufran@gmail.com>
2020-10-11 18:00:43 +0800

    Change image size to 100%

--- src/Soal4/Soal4.js
+++ src/Soal4/Soal4.js
@@ -5,7 +5,7 @@ export default function Soal4() {
    return (
       <div className="container">
          <h1>Point of Sales App Entitiy Relationship Diagram</h1>
-         <img src={erd} alt="ERD" width="1000" className="block-center"/>
+         <img src={erd} alt="ERD" width="100%" className="block-center"/>
       </div>
    )
 }
