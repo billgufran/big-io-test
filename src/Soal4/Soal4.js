a6a9566150bad21fdf6759294e8d568702db5bec
billgufran <billgufran@gmail.com>
2020-10-11 16:06:33 +0800

    Finished soal 4

---
+++ src/Soal4/Soal4.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import erd from './pos-erd.png'
+
+export default function Soal4() {
+   return (
+      <div className="container">
+         <h1>Point of Sales App Entitiy Relationship Diagram</h1>
+         <img src={erd} alt="ERD" width="1000" className="block-center"/>
+      </div>
+   )
+}
