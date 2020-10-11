c775f698c2e9c4823a491636bd23aa305c940a15
billgufran <billgufran@gmail.com>
2020-10-11 17:11:28 +0800

    Store data state to local storage

--- src/Soal5/Components/DataContext.js
+++ src/Soal5/Components/DataContext.js
@@ -1,9 +1,34 @@
 import React, { createContext, useState } from "react";
 
+//Custom hooks to set localStorage
+function useLocalStorage(key, initialValue) {
+	const [storedValue, setStoredValue] = useState(() => {
+		 try {
+			  const item = window.localStorage.getItem(key);
+			  return item ? JSON.parse(item) : initialValue;
+		 } catch (error) {
+			  console.log(error);
+			  return initialValue;
+		 }
+	});
+
+	const setValue = value => {
+		 try {
+			  const valueToStore =
+					value instanceof Function ? value(storedValue) : value;
+			  setStoredValue(valueToStore);
+			  window.localStorage.setItem(key, JSON.stringify(valueToStore));
+		 } catch (error) {
+			  console.log(error);
+		 }
+	};
+	return [storedValue, setValue];
+}
+
 export const DataContext = createContext();
 
 export default function DataProvider(props) {
-   const [data, setData] = useState([]);
+   const [data, setData] = useLocalStorage("pricesData", []);
 
 	const value = {data, setData};
 
