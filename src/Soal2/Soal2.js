8dc424cdce75581348b0a0f99743d6a65d94fc47
billgufran <billgufran@gmail.com>
2020-10-11 18:00:24 +0800

    Add algorithm description

--- src/Soal2/Soal2.js
+++ src/Soal2/Soal2.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import flowchart from './flowchart.png';
+import './Soal2.css';
 
 export default function Soal2() {
 
@@ -33,11 +34,25 @@ export default function Soal2() {
          <h1>Pattern Print Algorithm</h1>
          <h2>Flowchart</h2>
          <hr/>
-         <img src={flowchart} alt="Flowchart" width="900" className="block-center"/>
+         <img src={flowchart} alt="Flowchart" width="100%" className="block-center"/>
+         <h2>Description</h2>
+         <hr/>
+         <ol>
+            <li>Ask for user with integer data type</li>
+            <li>Accept user input and assign it to local variable named <code>size</code></li>
+            <li>Initialize <code>rows = (size * 2) - 1</code> and <code>i = size</code></li>
+            <li>Initialize <code>j = 1</code> and <code>k = size - 1</code></li>
+            <li>While <code>k != 0</code>, keep printing white space (<code>' '</code>) and decrement <code>k</code> until <code>k == 0</code></li>
+            <li>If <code>k == 0</code>, print asterisk and whitespace (<code>'* '</code>) and increment <code>j</code> while <code>j != i</code></li>
+            <li>If <code>j == i</code>, check whether <code>rows &lt;= 0</code>. If yes, the program <b>ends here</b>. If no, continue to the next step</li>
+            <li>Check whether <code>rows &lt;= size</code>. If yes, increment <code>i</code>. If no, decrement <code>i</code></li>
+            <li>Decrement rows and print line break</li>
+            <li>Repeat from step 4 until the program is ended</li>
+         </ol>
          <h2>Pseudocode</h2>
          <hr/>
          <pre>
-            <code>
+            <code id="pseudocode">
                {pseudocode}
             </code>
          </pre>
