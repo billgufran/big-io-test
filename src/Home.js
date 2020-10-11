fef34a8e80ebbe8a9c92b17be761b10ad90ff896
billgufran <billgufran@gmail.com>
2020-10-11 16:06:10 +0800

    Add homepage

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Stylesheet/Home.css'
+
+export default function Home() {
+   return (
+      <div id="Home">
+         <h1>Welcome</h1>
+         <p>Please select the question's number from the sidebar to reveal the answer</p>
+         <p>Sincerely, <a href="https://github.com/billgufran" target="_blank" rel="noopener noreferrer">Muhammad Fikri Bill Gufran</a></p>
+      </div>
+   )
+}
