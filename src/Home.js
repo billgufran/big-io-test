17a478a03c35f70836daf7c5922e75d617fcc99f
billgufran <billgufran@gmail.com>
2020-10-11 17:59:36 +0800

    Edit text

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
    return (
       <div id="Home">
          <h1>Welcome</h1>
-         <p>Please select the question's number from the sidebar to reveal the answer</p>
+         <p>Please select the question's number from the sidebar to reveal the answer.</p>
          <p>Sincerely, <a href="https://github.com/billgufran" target="_blank" rel="noopener noreferrer">Muhammad Fikri Bill Gufran</a></p>
       </div>
    )
